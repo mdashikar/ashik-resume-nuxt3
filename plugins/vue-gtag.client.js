bfa4b364829f797a42fb9256c96ed6fa7be08381
Md. Ashiqur Rahman <md.ashik.ar@gmail.com>
2023-09-17 02:47:20 +0600

    chore: upgrade nuxt version

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,9 +1,12 @@
-import VueGtag from 'vue-gtag-next'
+import VueGtag, { trackRouter } from 'vue-gtag-next'
+
 
 export default defineNuxtPlugin((nuxtApp) => {
+
   nuxtApp.vueApp.use(VueGtag, {
     property: {
       id: 'G-KL523KK9E9'
     }
   })
+  trackRouter(useRouter())
 })
